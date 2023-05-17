e3c429c94bb66aa7e0452874ebd2df10012c4f8c
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-05-17 13:06:09 +0530

    Added: MainContent composable

--- app/src/main/java/com/backtocding/tipapp/MainActivity.kt
+++ app/src/main/java/com/backtocding/tipapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.backtocding.tipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,18 +13,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.backtocding.tipapp.components.InputField
 import com.backtocding.tipapp.ui.theme.TipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +38,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+//                TopHeader()
+                MainContent()
             }
         }
     }
@@ -52,7 +60,6 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
 @Composable
 fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(
@@ -88,6 +95,43 @@ fun GreetingPreview() {
     TipAppTheme {
         MyApp {
             Text("Hello Again")
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),
+    shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column() {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) {
+                        return@KeyboardActions
+                    } else {
+                        keyboardController?.hide()
+                    }
+                }
+            )
         }
     }
 }
